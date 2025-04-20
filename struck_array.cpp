4a58fab55ef6f08701ccf34146d6fb8bc72262af
Kyosohee18 <hanashahky@gmail.com>
2025-04-20 16:51:02 +0700

    display data

--- struck_array.cpp
+++ struck_array.cpp
@@ -20,4 +20,15 @@ int main()
     cin >> mhs[i].nama;
     cout << "Masukan alamat = ";
     cin >> mhs[i].alamat;
+} 
+cout << endl;
+cout << "Menampilkan Data" << endl;
+cout << endl;
+
+for (int i = 0; i < 4; i++)
+{
+    cout << "Mahasiswa ke-" << i + 1 << endl;
+    cout << "NIM = " << mhs[i].nim << endl;
+    cout << "Nama = " << mhs[i].nama << endl;
+    cout << "Alamat = " << mhs[i].alamat << endl;
 }
