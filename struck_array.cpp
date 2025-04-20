8425cf91d3bb7d60e10dfbbe86d6a3bc8556f50f
Kyosohee18 <hanashahky@gmail.com>
2025-04-20 16:53:32 +0700

    Display Data Using Loop

--- struck_array.cpp
+++ struck_array.cpp
@@ -32,3 +32,15 @@ for (int i = 0; i < 4; i++)
     cout << "Nama = " << mhs[i].nama << endl;
     cout << "Alamat = " << mhs[i].alamat << endl;
 }
+cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "Alamat = " << mhs[i].alamat << endl;
+    }
+}
+
