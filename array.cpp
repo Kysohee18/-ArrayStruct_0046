46186802fb83cca059e73c720e304ca8fc531d22
Kyosohee18 <hanashahky@gmail.com>
2025-04-20 16:34:03 +0700

    Output Formatting

--- array.cpp
+++ array.cpp
@@ -7,7 +7,7 @@ int nilai[] = {1, 2, 4, 7};
 // deklarasi array dengan ukuran dan diberi nilai awal
 int nilaiDua[3] = {1, 2, 3};
 
-/ deklarasi array dengan ukuran, tanpa nilai awal
+// deklarasi array dengan ukuran, tanpa nilai awal
 string nama[4];
 
 int main()
@@ -15,13 +15,25 @@ int main()
     cout << "Nilai array ke 3 = " << nilai[2] << endl;
     nilai[2] = 100;
     cout << "Nilai array ke 3 = " << nilai[2] << endl;
-    
-   // Mengisi array dengan menggunakan looping for
-   for (int i = 0; i < 4; i++)
-   {
-       cout << "Masukan nama ke " << i + 1 << " = ";
-       cin >> nama[i];
-   }
+
+    // mengisi array dengan menggunakan looping for
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Masukan nama ke " << i + 1 << " = ";
+        cin >> nama[i];
+    }
+
+    cout << endl;
+    cout << "Menampilkan Array Nama" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Nama ke " << i + 1 << " = " << nama[i] << endl;
+    }
+}
+
+
 
 
 
