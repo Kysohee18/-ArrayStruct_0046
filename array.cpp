346114bbcc7aedb0a89b950ab05f7bc12301c56c
Kyosohee18 <hanashahky@gmail.com>
2025-04-20 16:31:24 +0700

    Add Loop for User Input

--- array.cpp
+++ array.cpp
@@ -15,12 +15,13 @@ int main()
     cout << "Nilai array ke 3 = " << nilai[2] << endl;
     nilai[2] = 100;
     cout << "Nilai array ke 3 = " << nilai[2] << endl;
+    
+   // Mengisi array dengan menggunakan looping for
+   for (int i = 0; i < 4; i++)
+   {
+       cout << "Masukan nama ke " << i + 1 << " = ";
+       cin >> nama[i];
+   }
 
-    // mengisi array dengan menggunakan looping for
-    for (int i = 0; i < 4; i++)
-    {
-        cout << "Masukan nama ke " << i + 1 << " = ";
-        cin >> nama[i];
-    }
 
 
