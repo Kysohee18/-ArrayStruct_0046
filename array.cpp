d300028e20f0431a02f461223cdb9e20d9e174b0
Kyosohee18 <hanashahky@gmail.com>
2025-04-20 16:29:04 +0700

    Display Array Values

--- array.cpp
+++ array.cpp
@@ -7,5 +7,20 @@ int nilai[] = {1, 2, 4, 7};
 // deklarasi array dengan ukuran dan diberi nilai awal
 int nilaiDua[3] = {1, 2, 3};
 
+/ deklarasi array dengan ukuran, tanpa nilai awal
+string nama[4];
+
+int main()
+{
+    cout << "Nilai array ke 3 = " << nilai[2] << endl;
+    nilai[2] = 100;
+    cout << "Nilai array ke 3 = " << nilai[2] << endl;
+
+    // mengisi array dengan menggunakan looping for
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Masukan nama ke " << i + 1 << " = ";
+        cin >> nama[i];
+    }
 
 
