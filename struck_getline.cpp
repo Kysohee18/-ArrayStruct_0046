bf8540fb840e9b9bb3ae447d5b9a3c873a4fd091
Kyosohee18 <hanashahky@gmail.com>
2025-04-20 17:01:21 +0700

    Define Structures and Arra

---
+++ struck_getline.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa
+{
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};
+
+Mahasiswa mhs[2]; // Array untuk menyimpan 2 data mahasiswa
